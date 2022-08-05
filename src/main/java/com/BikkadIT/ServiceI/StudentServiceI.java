6a8323dab7c8499ce7a2fcacc35430678759a32c
dhanshreederle123 <dhanshreederle@gmail.com>
2022-08-05 17:32:24 +0530

    sms-02:ServiceI interface created

---
+++ src/main/java/com/BikkadIT/ServiceI/StudentServiceI.java
@@ -0,0 +1,15 @@
+package com.BikkadIT.ServiceI;
+
+import java.util.List;
+
+import com.BikkadIT.model.Student;
+
+public interface StudentServiceI {
+public int addStudent(Student student);
+	
+	public List<Student> getAllStudent();
+	
+	public Student getStudentById(Integer id);
+
+
+}
